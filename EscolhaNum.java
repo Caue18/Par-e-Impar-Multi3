1415a194688800ca90577822e8798c5e3f342ba0
Cauê Souza <107008671+Caue18@users.noreply.github.com>
2024-02-21 10:32:58 -0300

    Par, Impar e Multiplos de 3 Python e Java

---
+++ EscolhaNum.java
@@ -0,0 +1,41 @@
+import java.util.Random; // Importação Aletório
+import java.util.ArrayList; // Importação  Lista Encadenada
+import java.util.List; // Importação Lista
+import java.util.Scanner; // Importação leitura de dados do usuário
+
+// Classe Escolha Aletório
+public class EscolhaNum {
+    public static List<Integer> gerarListaRandom(int size) {
+        List<Integer> listaRandom = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            listaRandom.add(random.nextInt(100) + 1); // Adiciona um número aleatório entre 1 e 100 à lista
+        }
+        return listaRandom;
+    }
+// Função Par, Impar e Multiplos de 3
+    public static void verificarNumero(int numero) {
+        System.out.println("---Resultado----");
+        if (numero % 3 == 0) { // Verifica se o número é múltiplo de 3
+            System.out.println(numero + " É multiplo de 3");
+        }
+        if (numero % 2 == 0) { // Verifica se o número é par
+            System.out.println(numero + " É Par");
+        } else { // Se não for par, então é ímpar
+            System.out.println(numero + " É Impar");
+        }
+    }
+// Função Principal
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o Tamanho da lista");
+        int size = scanner.nextInt();
+        List<Integer> listaRandom = gerarListaRandom(size); //  Chama função para gerar o resultado 
+        System.out.println("Lista foi Gerada: " + listaRandom); // mostra a lista 
+
+        for (int numero : listaRandom) {
+            verificarNumero(numero);
+        }
+    }
+}
---
+++ par_Impar.py
@@ -0,0 +1,27 @@
+import random  # Importação aleatória
+
+# Função de aleatório
+def lista_Gerada_random(size):
+    lista_random = [random.randint(1, 100) for _ in range(size)]
+    return lista_random
+
+# Função de números escolha de par, ímpar e múltiplos de 3
+print("---Resultado----")
+def num_escolha(num): # Função  para escolher um número
+    if num % 3 == 0:  # Se o número for múltiplo de 3
+        print(f"{num} é múltiplo de 3")
+    if num % 2 == 0:  # Se o número for par
+        print(f"{num} é Par")
+    else: # se For Impar
+        print(f"{num} é Ímpar")
+
+# Função Principal
+def principal():
+    size = int(input("Digite o Tamanho da lista: ")) # Digite um número
+    lista_random = lista_Gerada_random(size) # gera uma lista com números aletorios
+    print("Lista foi Gerada:", lista_random) # mensagem: "Lista foi gerada"
+    for num in lista_random: #  Percorre  os valores da lista
+        num_escolha(num)  #Chama a função de escolha de numero
+
+if __name__ == "__main__": #  condição principal do programa
+    principal()
